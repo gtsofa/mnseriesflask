951fadd70c7a8b80c0c0523e77dc8b548869777e
gtsofa <jlsnyule@yahoo.com>
2018-04-28 13:23:25 +0300

    refactor simple app without blueprint

---
+++ simple_wo_blueprint_example/myapp/models.py
@@ -0,0 +1,9 @@
+# myapp/models.py
+from myapp import db
+
+# define model here
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    random = db.Column(db.Integer)
--- simple_wo_blueprint_example/myapp/myapp.py
+++ simple_wo_blueprint_example/myapp/myapp.py
@@ -5,7 +5,17 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 
+# from views import * : mistake initializing views before app won;t run
+
+# initialize the db
+db = SQLAlchemy()
+
 app = Flask(__name__)
+
+# load configs from python file ..config.py
+app.config.from_pyfile('config.py')
+
+from views import *
 
 if __name__ == '__main__':
     app.run()
