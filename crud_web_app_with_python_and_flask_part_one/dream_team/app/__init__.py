d74281799beffbc0602fc15e96203c081dac3c7a
gtsofa <jlsnyule@yahoo.com>
2018-05-07 21:45:47 +0300

    add views for custom error pages

--- crud_web_app_with_python_and_flask_part_one/dream_team/app/__init__.py
+++ crud_web_app_with_python_and_flask_part_one/dream_team/app/__init__.py
@@ -56,7 +56,7 @@
     # app/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -80,6 +80,18 @@ def create_app(config_name):
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
 
     from app import models
 
